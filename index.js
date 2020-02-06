2c7ea34543ce7225eb0d54e70f7b0cfd47205a42
José Antonio Tarifa Lorenzo <jatarifa@users.noreply.github.com>
2020-02-06 23:19:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const dateUp = Date.now();
 
 app.get('/', (req, res) => {
   const today = new Date();
-
+  console.log(req.headers);
+  console.log(req.headers)
   res.json({
     date: today,
     up: `${(Date.now() - dateUp)/1000} seg. esto esta que te cagas`,
